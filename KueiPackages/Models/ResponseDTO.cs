e84e157a80c70bf5c6a98ce06afc780f0d8a4be4
Kuei <ragnakuei@livemail.tw>
2022-04-29 09:44:47 +0800

    Update ResponseDTO

--- KueiPackages/Models/ResponseDTO.cs
+++ KueiPackages/Models/ResponseDTO.cs
@@ -26,6 +26,37 @@ public abstract class BaseResponseDTO
                    AlertMessage = alertMessage,
                };
     }
+
+    /// <summary>
+    /// 一頁幾筆資料
+    /// </summary>
+    public int? PageSize { get; set; }
+
+    /// <summary>
+    /// 目前頁數
+    /// </summary>
+    public int? PageNo { get; set; }
+
+    /// <summary>
+    /// 資料總筆數
+    /// </summary>
+    public int DataCount { get; set; }
+    
+    /// <summary>
+    /// 總頁數
+    /// </summary>
+    public int? PageCount
+    {
+        get
+        {
+            if (DataCount % PageSize == 0)
+            {
+                return DataCount / PageSize;
+            }
+
+            return (DataCount / PageSize) + 1;
+        }
+    }
 }
 
 public class ResponseDTO : BaseResponseDTO
